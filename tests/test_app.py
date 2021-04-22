2ed39cd84fee00661efd323085ab82698c1997f0
Richard Lin <richard.lin@datasociety.com>
2021-04-22 16:36:01 -0400

    added circleci config file

---
+++ tests/test_app.py
@@ -0,0 +1,30 @@
+import pytest 
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+
+import sys
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.keys import Keys
+from time import sleep
+
+
+
+def test_app():
+    chrome_driver = webdriver.Chrome(ChromeDriverManager().install())
+
+    chrome_driver.get('http://127.0.0.1:5000/')
+    chrome_driver.maximize_window()
+ 
+    chrome_driver.find_element_by_name("title").send_keys("first test")
+    chrome_driver.find_element_by_xpath("//form[@class='ui form']/button[1]").click()
+    chrome_driver.find_element_by_name("title").send_keys("second test")
+    chrome_driver.find_element_by_xpath("//form[@class='ui form']/button[1]").click()
+
+    num_elements = 2
+    sleep(3)
+    # arr = chrome_driver.find_elements_by_class_name("ui segment")
+    arr = chrome_driver.find_elements_by_xpath("//div[@class='ui segment']")
+    assert len(arr) == num_elements
+    chrome_driver.close()
+
+    assert len(arr) == num_elements    
